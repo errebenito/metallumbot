1f5bb609fe9d00ff7738163d958728d715c26364
Raúl Benito <erre.benito@gmail.com>
2026-02-14 18:34:41 +0100

    test: improve coverage

--- src/test/java/com/github/errebenito/metallumbot/bot/CommandProcessorTest.java
+++ src/test/java/com/github/errebenito/metallumbot/bot/CommandProcessorTest.java
@@ -3,6 +3,11 @@ package com.github.errebenito.metallumbot.bot;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.junit.jupiter.api.Test;
 
 import com.github.errebenito.metallumbot.model.Command;
@@ -41,5 +46,37 @@ class CommandProcessorTest {
         processor.process(new Command("42", "/start"));
 
         assertNull(fakeSender.lastText);
+    }
+
+    @Test
+    void shouldLogWhenSendMessageThrows() {
+
+        CommandHandler fakeUseCase =
+            () -> "https://www.metal-archives.com/bands/Test/123";
+
+        FakeTelegramSender fakeSender = new FakeTelegramSender(true);
+
+
+        CommandProcessor processor =
+            new CommandProcessor(fakeUseCase, fakeSender);
+
+        Logger logger = LogManager.getLogger(CommandProcessor.class);
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        LoggerConfig loggerConfig = context.getConfiguration().getLoggerConfig(logger.getName());
+
+        ListAppender listAppender = ListAppender.create("TestAppender");
+        listAppender.start();
+        loggerConfig.addAppender(listAppender, null, null);
+        context.updateLoggers();
+
+        processor.process(new Command("42", "/band"));
+
+        assertEquals(1, listAppender.getEvents().size());
+
+        String loggedMessage = listAppender.getEvents().get(0).getMessage().getFormattedMessage();
+        assertEquals("simulated failure", loggedMessage);
+
+        loggerConfig.removeAppender("TestAppender");
+        listAppender.stop();
     }
 }
--- src/test/java/com/github/errebenito/metallumbot/bot/FakeTelegramSender.java
+++ src/test/java/com/github/errebenito/metallumbot/bot/FakeTelegramSender.java
@@ -1,11 +1,25 @@
 package com.github.errebenito.metallumbot.bot;
 
+import org.checkerframework.checker.units.qual.s;
+
 public class FakeTelegramSender implements TelegramSender {
     String lastChatId;
     String lastText;
+    private final boolean shouldThrow;
+
+    public FakeTelegramSender() {
+        this(false);
+    }
+
+    public FakeTelegramSender(boolean shouldThrow) {
+        this.shouldThrow = shouldThrow;
+    }
 
     @Override
     public void sendMessage(String chatId, String text) {
+        if (shouldThrow) {
+            throw new RuntimeException("simulated failure");
+        }
         this.lastChatId = chatId;
         this.lastText = text;
     }
