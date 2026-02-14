7e9239e59bdb6bb3e79622d41ee3ff213d35acc1
Raúl Benito <erre.benito@gmail.com>
2026-02-14 18:12:28 +0100

    chore: cleanup

--- src/main/java/com/github/errebenito/metallumbot/TelegramBotsLongPollingApplicationFactory.java
+++
@@ -1,7 +0,0 @@
-package com.github.errebenito.metallumbot;
-
-import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
-
-public interface TelegramBotsLongPollingApplicationFactory {
-  TelegramBotsLongPollingApplication create();
-}
