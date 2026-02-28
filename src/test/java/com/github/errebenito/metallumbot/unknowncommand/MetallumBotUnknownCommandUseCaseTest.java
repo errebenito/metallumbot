30b043f4de3ddcbb59511041cc8c206e163dd2cc
Raúl Benito <erre.benito@gmail.com>
2026-02-28 19:06:22 +0100

    test: One test per outcome. Test have names following the given-when-thenshould pattern. Tests use @Displayname

--- src/test/java/com/github/errebenito/metallumbot/unknowncommand/MetallumBotUnknownCommandUseCaseTest.java
+++ src/test/java/com/github/errebenito/metallumbot/unknowncommand/MetallumBotUnknownCommandUseCaseTest.java
@@ -2,6 +2,7 @@ package com.github.errebenito.metallumbot.unknowncommand;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.github.errebenito.metallumbot.shared.RecordingMessageSender;
@@ -9,7 +10,8 @@ import com.github.errebenito.metallumbot.shared.RecordingMessageSender;
 class MetallumBotUnknownCommandUseCaseTest {
 
     @Test
-    void shouldSendUsageInstructions() {
+    @DisplayName("Verifies that the use case delegates to the message sender")
+    void givenReceiverAndMessageSenderWhenReplyingToUnknownCommandThenShouldDelegateToSender() {
 
         var sender = new RecordingMessageSender();
         var useCase = new MetallumBotUnknownCommandUseCase(sender);
@@ -17,7 +19,29 @@ class MetallumBotUnknownCommandUseCaseTest {
         useCase.sendInstructions("42");
 
         assertTrue(sender.called);
+    }
+
+    @Test
+    @DisplayName("Verifies that the use case sends replies to the correct receiver")
+    void givenReceiverAndMessageSenderWhenReplyingToUnknownCommandThenShouldSendResponseToExpectedReceiver() {
+
+        var sender = new RecordingMessageSender();
+        var useCase = new MetallumBotUnknownCommandUseCase(sender);
+
+        useCase.sendInstructions("42");
+
         assertEquals("42", sender.receiver);
+    }
+
+    @Test
+    @DisplayName("Verifies that the use case replies to unknown commands by sending usage instructions")
+    void givenReceiverAndMessageSenderWhenReplyingToUnknownCommandThenShouldSendExpectedMessage() {
+
+        var sender = new RecordingMessageSender();
+        var useCase = new MetallumBotUnknownCommandUseCase(sender);
+
+        useCase.sendInstructions("42");
+
         assertEquals(MetallumBotUnknownCommandUseCase.USAGE,
             sender.message);
     }
