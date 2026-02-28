cf0d0848517d7fed7004ab6f8e8411a464500a4a
Raúl Benito <erre.benito@gmail.com>
2026-02-28 19:39:01 +0100

    test: One test per outcome. Test have names following the given-when-thenshould pattern. Tests use @Displayname

--- src/test/java/com/github/errebenito/metallumbot/upcomingalbum/MetalArchivesRandomUpcomingAlbumUseCaseTest.java
+++ src/test/java/com/github/errebenito/metallumbot/upcomingalbum/MetalArchivesRandomUpcomingAlbumUseCaseTest.java
@@ -2,13 +2,15 @@ package com.github.errebenito.metallumbot.upcomingalbum;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.github.errebenito.metallumbot.shared.RecordingMessageSender;
 
 class MetalArchivesRandomUpcomingAlbumUseCaseTest {
     @Test
-    void shouldSendFormattedAlbum() {
+    @DisplayName("Verifies that the use case delegates to the sender")
+    void givenReceiverAlbumProviderMessageSenderAndAlbumWhenSendingAlbumThenShouldDelegateToSender() {
 
         var album = new Album(
             "bandUrl",
@@ -27,12 +29,57 @@ class MetalArchivesRandomUpcomingAlbumUseCaseTest {
         useCase.sendUpcomingAlbum("42");
 
         assertTrue(sender.called);
+    }
+
+    @Test
+    @DisplayName("Verifies that the use case sends the album to the expected destination")
+    void givenReceiverAlbumProviderMessageSenderAndAlbumWhenSendingAlbumThenShouldSendToExpectedReceiver() {
+
+        var album = new Album(
+            "bandUrl",
+            "Band Name",
+            "albumUrl",
+            "Album Name",
+            "Full-length",
+            "Metal"
+        );
+
+        var provider = new StubUpcomingAlbumProvider(album);
+        var sender = new RecordingMessageSender();
+
+        var useCase = new MetalArchivesRandomUpcomingAlbumUseCase(provider, sender);
+
+        useCase.sendUpcomingAlbum("42");
+
         assertEquals("42", sender.receiver);
+    }
+
+    @Test
+    @DisplayName("Verifies that the use case sends the expected message")
+    void givenReceiverAlbumProviderMessageSenderAndAlbumWhenSendingAlbumThenShouldSendExpectedMessage() {
+
+        var album = new Album(
+            "bandUrl",
+            "Band Name",
+            "albumUrl",
+            "Album Name",
+            "Full-length",
+            "Metal"
+        );
+
+        var provider = new StubUpcomingAlbumProvider(album);
+        var sender = new RecordingMessageSender();
+
+        var useCase = new MetalArchivesRandomUpcomingAlbumUseCase(provider, sender);
+
+        useCase.sendUpcomingAlbum("42");
+
         assertEquals(album.toString(), sender.message);
     }
 
     @Test
-    void shouldSendFallbackMessageWhenProviderFails() {
+    @DisplayName("Verifies that when the provider throws any exception, the response contains an user-friendly error message")
+    void givenExceptionIsThrownWhenSendingAlbumThenShouldSendErrorMessage() {
 
         var provider = new ThrowingUpcomingAlbumProvider();
         var sender = new RecordingMessageSender();
@@ -41,12 +88,12 @@ class MetalArchivesRandomUpcomingAlbumUseCaseTest {
 
         useCase.sendUpcomingAlbum("42");
 
-        assertTrue(sender.called);
         assertEquals("Failed to retrieve upcoming album", sender.message);
     }
 
     @Test
-    void shouldReinterruptThreadWhenInterrupted() {
+    @DisplayName("Verifies that when the provider throws an InterruptedException, the response contains an user-friendly error message")
+    void givenInterruptedExceptionIsThrownWhenSendingAlbumThenShouldSendErrorMessage() {
 
         var provider = new InterruptingUpcomingAlbumProvider();
         var sender = new RecordingMessageSender();
@@ -57,8 +104,21 @@ class MetalArchivesRandomUpcomingAlbumUseCaseTest {
 
         useCase.sendUpcomingAlbum("42");
 
-        assertTrue(sender.called);
         assertEquals("Failed to retrieve upcoming album", sender.message);
+    }
+
+    @Test
+    @DisplayName("Verifies that when the provider throws an InterruptedException, the current thread is interrupted")
+    void givenInterruptedExceptionIsThrownWhenSendingAlbumThenShouldInterruptCurrentThread() {
+        var provider = new InterruptingUpcomingAlbumProvider();
+        var sender = new RecordingMessageSender();
+
+        var useCase = new MetalArchivesRandomUpcomingAlbumUseCase(provider, sender);
+
+        Thread.interrupted();
+
+        useCase.sendUpcomingAlbum("42");
+
         assertTrue(Thread.currentThread().isInterrupted());
     }
 }
